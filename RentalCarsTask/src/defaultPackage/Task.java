0088296cbceb0d46dbb65e681b059fec314423a4
O'Shea Douglas <shea_o@me.com>
2017-08-01 20:53:57 +0100

    Part 1 is complete, need to test and optimise

--- RentalCarsTask/src/defaultPackage/Task.java
+++ RentalCarsTask/src/defaultPackage/Task.java
@@ -4,7 +4,11 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.text.ParseException;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.Iterator;
+import java.util.Random;
 
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
@@ -12,12 +16,59 @@ import org.json.simple.parser.JSONParser;
 
 public class Task {
 	public static void main(String[] args) {
-		ArrayList<Vehicle> vehicleArray = new ArrayList<Vehicle>();
-		JSONObject j;
 		
+		// Read in JSON File
+		ArrayList<Vehicle> vehicleArray = parseJSONToArray("vehicles.json");
+		
+		// Task 1
+		Collections.sort(vehicleArray, new Comparator<Vehicle>(){
+			@Override public int compare(Vehicle v1, Vehicle v2){
+				return Double.compare(v1.returnPrice(), v2.returnPrice());
+			}
+		});
+		
+		for(int i = 0; i < vehicleArray.size(); i++){
+			System.out.println(vehicleArray.get(i).returnName() + " - " + vehicleArray.get(i).returnPrice());
+			}
+		
+		// Task 2
+		for(int i = 0; i < vehicleArray.size(); i++){
+			System.out.println(vehicleArray.get(i).returnName() + " - " + vehicleArray.get(i).returnSipp() + " - " + vehicleArray.get(i).returnCarType() + " - " + vehicleArray.get(i).returnDoorsCarType() + " - " + vehicleArray.get(i).returnTransmission() + " - " + vehicleArray.get(i).returnFuelAirCon());
+		}	
+		
+		// Task 3
+		Collections.sort(vehicleArray, new Comparator<Vehicle>(){
+			@Override public int compare(Vehicle v1, Vehicle v2){
+				return Double.compare(v2.returnRating(), v1.returnRating());
+			}
+		});
+		
+		for(int i = 0; i < vehicleArray.size(); i++){
+			System.out.println(vehicleArray.get(i).returnName() + " - " + vehicleArray.get(i).returnPrice() + " - " + vehicleArray.get(i).returnSupplier() + " - " + vehicleArray.get(i).returnRating());
+			}
+	
+		System.out.println("-------------");	
+		
+		// Task 4
+		Collections.sort(vehicleArray, new Comparator<Vehicle>(){
+			@Override public int compare(Vehicle v1, Vehicle v2){
+				return Double.compare(v2.returnSumOfScores(), v1.returnSumOfScores());
+			}
+		});
+		
+		for(int i = 0; i < vehicleArray.size(); i++){
+			System.out.println(vehicleArray.get(i).returnName() + " - " + vehicleArray.get(i).returnVehicleScore() + " - " + vehicleArray.get(i).returnRating() + " - " + vehicleArray.get(i).returnSumOfScores());
+			}
+		
+}
+	
+	
+	
+	private static ArrayList<Vehicle> parseJSONToArray(String path){
+		ArrayList<Vehicle> output = new ArrayList<Vehicle>();
 		JSONParser parser = new JSONParser(); 
 		try{
-			Object object = parser.parse(new FileReader("vehicles.json"));
+			Object object = parser.parse(new FileReader(path));
 			
 			JSONObject jsonObject = (JSONObject) object;
 			JSONObject search = (JSONObject) jsonObject.get("Search");
@@ -26,23 +77,110 @@ public class Task {
 			Vehicle v;
 			
 			for(int i = 0; i < vehicleList.size(); i++){
+				
 				vehicle = (JSONObject) vehicleList.get(i);
 				v = new Vehicle((String) vehicle.get("sipp"),(String) vehicle.get("name"),(double) vehicle.get("price"),(String) vehicle.get("supplier"),Double.parseDouble(vehicle.get("rating").toString()));
-				System.out.println(vehicle.get("rating").getClass());
-	
-				vehicleArray.add(v);
+				output.add(v);
+				
 			}
 		} 
 		catch(FileNotFoundException e) {e.printStackTrace();}
 		catch(IOException e) {e.printStackTrace();}
 		catch(Exception e) {e.printStackTrace();}
-	
-		System.out.println(vehicleArray.size());
 		
-		
-		for(int i = 0; i < vehicleArray.size(); i++){
-		System.out.println(vehicleArray.get(i).returnRating());
-		}
-	
+		return output;
 	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	/*
+	 * 
+	 * 
+	 * int[] arr = new int[10];
+	Random rand = new Random();
+	
+	for(int i = 0; i < arr.length; i++){
+		arr[i] = rand.nextInt(10) + 1;
+	}
+	 */
+	
+	
+	
+	
+	
+	
+	
+	/*private int[] sort(int arr[], int lo, int hi){
+		
+		if(hi - lo <= 0){
+			return arr;
+		} else {
+			int splitPoint = split(arr, lo, hi);
+			sort(arr,lo,splitPoint - 1);
+			sort(arr,splitPoint + 1,hi);
+		}
+		
+		
+		return arr;
+	}
+	
+	
+	
+	
+	private int[] sort(int [] arr, int arrLength){
+		
+		if(arrLength <= 1){
+			return arr;
+		} else {
+			sort(arr,0,arrLength - 1);
+		}
+		return arr;
+	}*/
+	
+	
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- RentalCarsTask/src/defaultPackage/Vehicle.java
+++ RentalCarsTask/src/defaultPackage/Vehicle.java
@@ -6,6 +6,8 @@ public class Vehicle {
 	double price;
 	String supplier;
 	double rating;
+	double vehicleScore;
+	double sumOfScores;
 	
 	public Vehicle(String s, String n, double p, String su, double r){
 		sipp = s;
@@ -13,6 +15,7 @@ public class Vehicle {
 		price = p;
 		supplier = su;
 		rating = r;
+		this.setVehicleScore();
 	}
 	
 	
@@ -35,4 +38,212 @@ public class Vehicle {
 	public double returnRating(){
 		return rating;
 	}
+	
+	public String returnCarType(){
+		String value = "";
+		
+		if(sipp.charAt(0) == 'M'){
+			value = "Mini";
+		} else if (sipp.charAt(0) == 'E') {
+			value = "Economy";
+		} else if (sipp.charAt(0) == 'C') {
+			value = "Compcact";
+		} else if (sipp.charAt(0) == 'I') {
+			value = "Intermediate";
+		} else if (sipp.charAt(0) == 'S') {
+			value = "Standard";
+		} else if (sipp.charAt(0) == 'F') {
+			value = "Full Size";
+		} else if (sipp.charAt(0) == 'P') {
+			value = "Premium";
+		} else if (sipp.charAt(0) == 'L') {
+			value = "Luxury";
+		} else if (sipp.charAt(0) == 'X') {
+			value = "Special";
+		}
+		
+		return value;
+	}
+	
+	public String returnDoorsCarType(){
+		String value = "";
+		
+		if(sipp.charAt(1) == 'B'){
+			value = "2 doors";
+		} else if (sipp.charAt(1) == 'C') {
+			value = "4 doors";
+		} else if (sipp.charAt(1) == 'D') {
+			value = "5 doors";
+		} else if (sipp.charAt(1) == 'W') {
+			value = "Estate";
+		} else if (sipp.charAt(1) == 'T') {
+			value = "Convertable";
+		} else if (sipp.charAt(1) == 'F') {
+			value = "SUV";
+		} else if (sipp.charAt(1) == 'P') {
+			value = "Pick Up";
+		} else if (sipp.charAt(1) == 'V') {
+			value = "Passenger Van";
+		}
+	
+		return value;
+	}
+	
+	public String returnTransmission(){
+		String value = "";
+		
+		if(sipp.charAt(2) == 'M'){
+			value = "Manual";
+		} else {
+			value = "Automatic";
+		}
+		
+		return value;
+	}
+	
+	public String returnFuelAirCon(){
+		String value = "";
+		
+		if(sipp.charAt(3) == 'N'){
+			value = "Petrol - No Air Conditioning";
+		} else {
+			value = "Petrol - Air Conditioning";
+		}
+		
+		return value; 
+	}
+	
+	public void setVehicleScore(){
+		double score = 0;
+		
+		if(this.returnTransmission() == "Manual"){
+			score++;
+		} else{
+			score += 5;
+		}
+		
+		if(this.returnFuelAirCon() == "Petrol - Air Conditioning"){
+			score += 2;
+		}
+		vehicleScore = score;
+		sumOfScores = score + rating;
+		
+	}
+	
+	public double returnVehicleScore(){
+		return vehicleScore;
+	}
+	
+	public double returnSumOfScores(){
+		return sumOfScores;
+	}
+	
+	public String returnSIPPSpec(){
+		String value = "";
+		String output = name + " - " + sipp + " - ";
+		
+		if(sipp.charAt(0) == 'M'){
+			value = "Mini";
+		} else if (sipp.charAt(0) == 'E') {
+			value = "Economy";
+		} else if (sipp.charAt(0) == 'C') {
+			value = "Compcact";
+		} else if (sipp.charAt(0) == 'I') {
+			value = "Intermediate";
+		} else if (sipp.charAt(0) == 'S') {
+			value = "Standard";
+		} else if (sipp.charAt(0) == 'F') {
+			value = "Full Size";
+		} else if (sipp.charAt(0) == 'P') {
+			value = "Premium";
+		} else if (sipp.charAt(0) == 'L') {
+			value = "Luxury";
+		} else if (sipp.charAt(0) == 'X') {
+			value = "Special";
+		}
+		
+		output = output + value + " - ";
+		
+		if(sipp.charAt(1) == 'B'){
+			value = "2 doors";
+		} else if (sipp.charAt(1) == 'C') {
+			value = "4 doors";
+		} else if (sipp.charAt(1) == 'D') {
+			value = "5 doors";
+		} else if (sipp.charAt(1) == 'W') {
+			value = "Estate";
+		} else if (sipp.charAt(1) == 'T') {
+			value = "Convertable";
+		} else if (sipp.charAt(1) == 'F') {
+			value = "SUV";
+		} else if (sipp.charAt(1) == 'P') {
+			value = "Pick Up";
+		} else if (sipp.charAt(1) == 'V') {
+			value = "Passenger Van";
+		}
+		
+		output = output + value + " - ";
+		
+		if(sipp.charAt(2) == 'M'){
+			value = "Manual";
+		} else {
+			value = "Automatic";
+		}
+		
+		output = output + value + " - ";
+		
+		if(sipp.charAt(3) == 'N'){
+			value = "Petrol - No Air Conditioning";
+		} else {
+			value = "Petrol - Air Conditioning";
+		}
+		
+		output = output + value;	
+		
+		return output;
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
